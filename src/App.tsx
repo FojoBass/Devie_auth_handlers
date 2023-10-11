f6d622f825fd4076344297bc52edfc98bdeda285
FojoBass <fojo4god@gmail.com>
2023-10-11 02:58:38 +0100

    Reset Password

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useReducer } from 'react';
 import './scss/main.scss';
 import {
   checkActionCode,
@@ -12,6 +12,9 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom';
+import { BsEyeSlash, BsEye } from 'react-icons/bs';
+
+// TODO SET UP THE TRY CATCH FOR RESETTING PASSWORD, AND WE SHOULD BE DONE WITH THIS MINI PROJ
 
 const App = () => {
   const router = createBrowserRouter(
@@ -36,6 +39,17 @@ const App = () => {
 
 export default App;
 
+interface InfoInt {
+  type: 'error' | 'success' | '';
+  msg: string;
+  state: boolean;
+}
+
+interface ActionInt {
+  type: 'ERROR' | 'SUCCESS' | 'RESET';
+  payload?: string;
+}
+
 const Root = () => {
   const getParameterByName = (name: string, url = window.location.href) => {
     const params = url.split('?')[1]?.split('&');
@@ -50,14 +64,55 @@ const Root = () => {
     return '';
   };
 
-  const mode = getParameterByName('mode');
-  // const mode: string = 'verifyEmail';
+  // const mode = getParameterByName('mode');
+  const mode: string = 'resetPassword';
   const actionCode = getParameterByName('oobCode');
   const continueUrl = getParameterByName('continueUrl');
-  const lang = getParameterByName('lang') || 'en';
 
   const [isVerifying, setIsVerifying] = useState(true);
   const [verifyFailed, setVerifyFailed] = useState(false);
+  const [pWord, setPword] = useState('');
+  const [conPword, setConPword] = useState('');
+  const [isShowPword, setIsShowPword] = useState(false);
+  const [isShowConPword, setIsShowConPword] = useState(false);
+  const [info, setInfo] = useReducer(reducer, {
+    state: false,
+    type: '',
+    msg: '',
+  });
+  const infoRef = useRef<HTMLParagraphElement | null>(null);
+  const infoWrapperRef = useRef<HTMLDivElement | null>(null);
+
+  const validatePassword = (): boolean => {
+    if (!pWord) setInfo({ type: 'ERROR', payload: 'Enter password' });
+
+    if (pWord.length < 8) {
+      setInfo({
+        type: 'ERROR',
+        payload: "Password's char should be at least 8",
+      });
+      return false;
+    }
+
+    // ! ENSURE TO CHANGE BACK TO STRONG PASSWORD AND FOR MAIN APP
+    if (regex.alphaNumeric.test(pWord)) return true;
+
+    setInfo({
+      type: 'ERROR',
+      payload:
+        'Password should contain at least one; uppercase letter, lowercase letter, digit, and spceial character',
+    });
+
+    return false;
+  };
+
+  const validateConPassword = (): boolean => {
+    if (conPword !== pWord)
+      setInfo({ type: 'ERROR', payload: 'Passwords do not match' });
+    else return true;
+
+    return false;
+  };
 
   const handleVerify = async () => {
     try {
@@ -69,6 +124,37 @@ const Root = () => {
       setIsVerifying(false);
     }
   };
+
+  const handleReset = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (validatePassword() && validateConPassword()) {
+      console.log('valid passwords');
+    }
+  };
+
+  useEffect(() => {
+    if (infoRef.current && infoWrapperRef.current) {
+      const infoEl = infoRef.current;
+      const infoWrapperEl = infoWrapperRef.current;
+
+      if (info.state)
+        infoWrapperEl.style.height =
+          infoEl.getBoundingClientRect().height + 'px';
+      else infoWrapperEl.style.height = '0px';
+    }
+  }, [infoRef, infoWrapperRef, info]);
+
+  useEffect(() => {
+    let infoTimer: NodeJS.Timer;
+    if (info.state) {
+      infoTimer = setTimeout(() => {
+        setInfo({ type: 'RESET' });
+      }, 3000);
+
+      return () => clearTimeout(infoTimer);
+    }
+  }, [info]);
 
   useEffect(() => {
     switch (mode) {
@@ -89,7 +175,60 @@ const Root = () => {
     <section>
       <div className='center_sect'>
         {mode === 'resetPassword' ? (
-          <></>
+          <>
+            <h2 className='sect_heading'>Enter New Password</h2>
+            <form onSubmit={handleReset}>
+              <div className='form_opts'>
+                <div className='form_opt'>
+                  <input
+                    type={`${isShowPword ? 'text' : 'password'}`}
+                    placeholder='New Password'
+                    value={pWord}
+                    onChange={(e) => setPword(e.target.value)}
+                  />
+                  <button
+                    type='button'
+                    onClick={() => setIsShowPword(!isShowPword)}
+                  >
+                    {isShowPword ? <BsEye /> : <BsEyeSlash />}
+                  </button>
+                </div>
+
+                <div className='form_opt'>
+                  <input
+                    type={`${isShowConPword ? 'text' : 'password'}`}
+                    placeholder='Confirm Password'
+                    value={conPword}
+                    onChange={(e) => setConPword(e.target.value)}
+                  />
+                  <button
+                    type='button'
+                    onClick={() => setIsShowConPword(!isShowConPword)}
+                  >
+                    {isShowConPword ? <BsEye /> : <BsEyeSlash />}
+                  </button>
+                </div>
+              </div>
+
+              <button className='reset_btn'>Reset</button>
+
+              <div
+                className='info_wrapper'
+                ref={infoWrapperRef}
+                style={
+                  info.type === 'error'
+                    ? { backgroundColor: '#ff000027', color: '#ff0000' }
+                    : info.type === 'success'
+                    ? { backgroundColor: '#00800027', color: '#008800' }
+                    : {}
+                }
+              >
+                <p className='info' ref={infoRef}>
+                  {info.msg}
+                </p>
+              </div>
+            </form>
+          </>
         ) : mode === 'verifyEmail' ? (
           <>
             <h2 className='sect_heading'>Email Verification</h2>
@@ -115,4 +254,30 @@ const Root = () => {
       </div>
     </section>
   );
+};
+
+const reducer = (info: InfoInt, action: ActionInt): InfoInt => {
+  switch (action.type) {
+    case 'ERROR':
+      return { ...info, state: true, msg: action.payload ?? '', type: 'error' };
+    case 'SUCCESS':
+      return {
+        ...info,
+        state: true,
+        msg: action.payload ?? '',
+        type: 'success',
+      };
+    case 'RESET':
+      return { ...info, state: false };
+    default:
+      return info;
+  }
+};
+
+export const regex = {
+  alpha: /^[A-Za-z\s]+$/,
+  alphaNumeric: /^[a-zA-Z0-9]+$/,
+  email: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+  url: /^(http|https|ftp):\/\/([^\s/$.?#].[^\s]*)?$/,
+  strongPword: /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/,
 };
